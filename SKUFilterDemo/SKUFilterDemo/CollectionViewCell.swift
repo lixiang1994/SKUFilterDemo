5a208df0846b748f7f6acd171c3f6ed60c3ef591
lixiang1994 <18611401994@163.com>
2019-01-07 13:36:22 +0800

    优化UI细节

--- SKUFilterDemo/SKUFilterDemo/CollectionViewCell.swift
+++ SKUFilterDemo/SKUFilterDemo/CollectionViewCell.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label: UILabel! {
+        didSet {
+            label.layer.masksToBounds = true
+            label.layer.cornerRadius = 2
+            label.layer.borderWidth = 1
+        }
+    }
     
     func set(text: String?) {
         label.text = text
     }
     
     func set(enabled: Bool) {
-        label.textColor = enabled ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        label.textColor = enabled ? #colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1) : #colorLiteral(red: 0.7098039216, green: 0.7098039216, blue: 0.7098039216, alpha: 1)
+        label.backgroundColor = #colorLiteral(red: 0.9647058824, green: 0.9647058824, blue: 0.9647058824, alpha: 1)
+        label.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
     }
     
     func set(selected: Bool) {
-        label.textColor = selected ? #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1) : label.textColor
+        label.textColor = selected ? #colorLiteral(red: 1, green: 0.3137254902, blue: 0, alpha: 1) : label.textColor
+        label.backgroundColor = selected ? #colorLiteral(red: 1, green: 0.9725490196, blue: 0.9764705882, alpha: 1) : #colorLiteral(red: 0.9647058824, green: 0.9647058824, blue: 0.9647058824, alpha: 1)
+        label.layer.borderColor = selected ? #colorLiteral(red: 1, green: 0.3137254902, blue: 0, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
     }
 }
